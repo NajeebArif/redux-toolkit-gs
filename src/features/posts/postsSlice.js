1b41d2ec2f7579610bf3d0c989fefaf76bfd1863
Najeeb Arif <NajeebArif786@outlook.com>
2023-07-03 23:57:01 +0530

    Make IDs as string for correct comparision

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
-    {id: 1, title: 'First Post', content: 'Hello'},
-    {id: 2, title: 'Second Post', content: 'More Text'}
+    {id: '1', title: 'First Post', content: 'Hello'},
+    {id: '2', title: 'Second Post', content: 'More Text'}
 ];
 
 const postSlice = createSlice({
