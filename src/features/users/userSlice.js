fd4f41c78bc61136a407d294a23cd253d01a4237
Najeeb Arif <NajeebArif786@outlook.com>
2023-07-04 15:33:46 +0530

    Update userSlice.js

--- src/features/users/userSlice.js
+++ src/features/users/userSlice.js
@@ -1,14 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
 
-const initialState = [
-    {id: '1', name: 'Najeeb'},
-    {id: '2', name: 'Arif'}
-]
+const initialState = []
 
-const usersSlice = createSlice({
-    name: 'users',
-    initialState,
-    reducers: {}
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await client.get('/fakeApi/users')
+  return response.data
 })
 
-export default usersSlice.reducer;
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload
+    })
+  },
+})
+
+export default usersSlice.reducer
